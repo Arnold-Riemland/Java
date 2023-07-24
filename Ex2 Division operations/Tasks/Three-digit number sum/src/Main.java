724c5bc5488cdfcda3f9379866660c3b2de0d373
Arnold Riemland <arnold.riemland@smail.th-koeln.de>
2023-07-24 14:14:07 +0200

    Three-digit number sum complete

--- Ex2 Division operations/Tasks/Three-digit number sum/src/Main.java
+++ Ex2 Division operations/Tasks/Three-digit number sum/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /**
          * Пользователь вводит трёхзначное число.
          * Необходимо найти сумму его цифр.
          */
+        int nummer, a, b, c;
+        Scanner input = new Scanner(System.in);
+        nummer = input.nextInt();
+        a = nummer / 100;//3
+        b = nummer % 100 / 10;//6
+        c = nummer % 10;
+
+        System.out.printf("Sum = %d", a + c + b);
     }
 }
