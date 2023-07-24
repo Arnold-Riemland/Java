9196bd52f47fad41c29b574bcd38e306b7e12c38
Андрей Боев <a.boev.it@gmail.com>
2023-07-24 14:29:27 +0300

    Задачи Ex2

---
+++ Ex2 Division operations/Tasks/Three-digit number sum/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Пользователь вводит трёхзначное число.
+         * Необходимо найти сумму его цифр.
+         */
+    }
+}
