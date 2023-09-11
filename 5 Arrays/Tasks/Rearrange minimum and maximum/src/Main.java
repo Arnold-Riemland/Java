de1beb9698b32728730328a2b1ae0604ba867c4c
Андрей Боев <a.boev.it@gmail.com>
2023-09-11 15:14:52 +0300

    Проверка задачи "Обмен max и min"

--- 5 Arrays/Tasks/Rearrange minimum and maximum/src/Main.java
+++ 5 Arrays/Tasks/Rearrange minimum and maximum/src/Main.java
@@ -6,43 +6,24 @@ import static java.lang.Math.min;
 public class Main {
     public static void main(String[] args) {
         int number;
-        int sum = 0;
-        int mi = 1000;
-        int a = 0;
-        int b = 0;
-        int ma = 0;
-        int c = 0;
-        int d = 0;
-        int x;
+        int tmp, maxPos = 0, minPos = 0;
         Scanner input = new Scanner(System.in);
         number = input.nextInt();
         int[] array = new int[number];
-        if (number != sum) {
-            for (int i = 0; i < number; i++) {
-                array[i] = input.nextInt();
-                sum++;
+        for (int i = 0; i < number; i++) {
+            array[i] = input.nextInt();
+            if (array[i] > array[maxPos]) {
+                maxPos = i;
+            }
+            if (array[i] < array[minPos]) {
+                minPos = i;
             }
         }
+        tmp = array[maxPos];
+        array[maxPos] = array[minPos];
+        array[minPos] = tmp;
         for (int i = 0; i < array.length; i++) {
-
-            ma = max(array[i], ma);
-            if (ma > c) {
-                a=i;
-            }
-            c = ma;
-        }
-        for (int i = 0; i < array.length; i++) {
-            mi = min(array[i], mi);
-            if (d > mi) {
-                b=i;
-            }
-            d = mi;
-        }
-        x=array[a];
-        array[a] = array[b];
-        array[b] = x;
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.printf("%d ", array[i]);
         }
     }
 }
