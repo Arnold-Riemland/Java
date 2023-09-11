d5aa39517fb1a1cbbfc8e46f619eb5ba919601b9
Андрей Боев <a.boev.it@gmail.com>
2023-09-11 15:30:17 +0300

    Проверка задачи "Циклический сдвиг вправо"

--- 5 Arrays/Tasks/Cyclic shift to the right/src/Main.java
+++ 5 Arrays/Tasks/Cyclic shift to the right/src/Main.java
@@ -1,45 +1,24 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int number;
-        int sum = 0;
-        int x;
-
+        int tmp;
         Scanner input = new Scanner(System.in);
         number = input.nextInt();
         int[] array = new int[number];
-        if (number != sum) {
-            for (int i = 0; i < number; i++) {
-                array[i] = input.nextInt();
-                sum++;
-            }
+        for (int i = 0; i < number; i++) {
+            array[i] = input.nextInt();
         }
-        x = input.nextInt();
-        for (int j = 0; j < x; j++) {
-            int a = 1;
-            int nextElement = 0;
-            int saveNextElement=0;
-            int v;
-            for (int i = 0; i < array.length ; i++) {
-                if (i < array.length-1) {
-                    if (array[a] == array[i]) {
-                        a++;
-                        nextElement++;
-                        v = array[nextElement];
-                        array[a] = saveNextElement;
-                        saveNextElement = v;
-                    }else{
-                        nextElement++;
-                        saveNextElement= array[nextElement];
-                    array[i + 1] = array[i];
-                   }
-                } else {
-                    array[0] = saveNextElement;
-                }
-            }
+
+        tmp = array[number - 1];
+        for (int i = number - 1; i > 0; i--) {
+            array[i] = array[i - 1];
         }
+        array[0] = tmp;
+
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.printf("%d ", array[i]);
         }
     }
 }
