ed9d9ffb70bfdff15816e02d5bbe47559984e686
Андрей Боев <a.boev.it@gmail.com>
2023-11-16 15:21:39 +0300

    Fix ДЗ

--- 7. Strings/Tasks/Caesar Cipher/src/Main.java
+++ 7. Strings/Tasks/Caesar Cipher/src/Main.java
@@ -5,42 +5,27 @@ public class Main {
 
     public static void main(String[] args) {
         int c = 0;
-        int a = 0;
-        int b = -1;
-        int num;
+        int key;
         String str;
         int res = 0;
         Scanner input = new Scanner(System.in);
         str = input.nextLine();
-        num = input.nextInt();
+        key = input.nextInt();
         int[] index1 = new int[str.length()];
         char[] letters = str.toCharArray();
         for (int i = 0; i < str.length(); i++) {
-            for (int j = 0; j < num; j++) {
-                a++;
-                if ((alphabet.indexOf(str.charAt(i)) + a) < 26) {
-                    res = alphabet.indexOf(str.charAt(i)) + a;
-                } else {
-                    res = b + 1;
-                    b++;
-                }
-            }
+            res = (alphabet.indexOf(str.charAt(i)) + key) % alphabet.length();
             letters[i] = alphabet.charAt(res);
             index1[i] = res;
-            res = 0;
-            b = -1;
-            a = 0;
         }
-        for (
-                int i = 0; i < str.length(); i++) {
-            System.out.printf("%s ", letters[i]);
+        for (int i = 0; i < str.length(); i++) {
+            System.out.printf("%s", letters[i]);
         }
         System.out.println(" \n");
         for (int i = 0; i < str.length(); i++) {
-            for (int j = 0; j < num; j++) {
-                if (index1[i] - a > 0) {
-                    res = index1[i] - a - 1;
-                    a++;
+            for (int j = 0; j < key; j++) {
+                if (index1[i] - j > 0) {
+                    res = index1[i] - j - 1;
                 } else {
                     c++;
                     res = 26 - c;
@@ -48,12 +33,10 @@ public class Main {
             }
             letters[i] = alphabet.charAt(res);
             res = 0;
-            a = 0;
             c = 0;
         }
-        for (
-                int i = 0; i < str.length(); i++) {
-            System.out.printf("%s ", letters[i]);
+        for (int i = 0; i < str.length(); i++) {
+            System.out.printf("%s", letters[i]);
         }
     }
 }
