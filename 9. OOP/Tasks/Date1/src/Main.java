edeb5e9979b0f6f864efefbfee2e759bc41d0b43
Arnold Riemland <arnold.riemland@smail.th-koeln.de>
2024-03-27 14:20:00 +0100

    Date

---
+++ 9. OOP/Tasks/Date1/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date("29.02.2024");
+        System.out.println(date);
+        date.AddDay();
+        System.out.println(date);
+
+
+    }
+}
